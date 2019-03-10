21f95033603d6dc0df588bba4aedf7fcee278465
earoeh <earoeh@uw.edu>
2019-03-10 11:43:57 -0700

    Update display format

--- app_ui.R
+++ app_ui.R
@@ -1,21 +1,35 @@
 library("shiny")
 
-app_ui <- fluidPage(
-  titlePanel("Global Economy and Education Trends"),
-  tabsetPanel(
-    tabPanel("Nordic Countries",
-             selectInput("nordic_country",
-                         label = "Nordic Country", 
-                         choices = list("All", "Denmark",
-                                        "Finland", "Iceland",
-                                        "Norway", "Sweden")),
-             selectInput("top_country",
-                         label = "Top Economy Country",
-                         choices = list("All", "Germany",
-                                        "India", "Japan",
-                                        "United Kingdom",
-                                        "United States")),
-             plotOutput("nordic_plot"),
-             plotOutput("top_plot"))
-  )
+app_ui <- navbarPage(
+  "Global Economy and Education Trends",
+  tabPanel("Home"),
+  tabPanel(
+    "Nordic Countries",
+    div(
+      style = "display:inline-block",
+      selectInput("nordic_country",
+        label = "Nordic Country",
+        choices = list(
+          "All", "Denmark",
+          "Finland", "Iceland",
+          "Norway", "Sweden"
+        )
+      )
+    ),
+    div(
+      style = "display:inline-block",
+      selectInput("top_country",
+        label = "Top Economy Country",
+        choices = list(
+          "All", "Germany",
+          "India", "Japan",
+          "United Kingdom",
+          "United States"
+        )
+      )
+    ),
+    plotOutput("nordic_plot"),
+    plotOutput("top_plot")
+  ),
+  tabPanel("About")
 )
