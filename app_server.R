90857b77f0e40ae956e6eae6a76c6d9d29c16495
mayasioson <mayas1@uw.edu>
2019-03-11 19:05:30 -0700

    Create plot for developing countries

--- app_server.R
+++ app_server.R
@@ -1,5 +1,6 @@
 library("shiny")
 source("scripts/nordic.R")
+source("scripts/developing.R")
 
 app_server <- function(input, output) {
   
@@ -11,4 +12,55 @@ app_server <- function(input, output) {
     generate_line_plot_top(input$top_country)
   })
   
+  output$nordic_description <- renderText({
+    message <- "The line plots"
+  })
+  
+  output$nordic_analysis <- renderText({
+    message <- "On average, a Nordic country spends "
+    message <- paste0(message,
+                      avg_nordic_expenditure_percent,
+                      "% of their total GDP on education, while a country with a top GDP spends ",
+                      avg_top_economy_expenditure_percent,
+                      "% of their total GDP.")
+    message <- paste(message,
+                     "From this, it seems as though",
+                     "the average Nordic country spends more on education",
+                     "than the average country with a top GDP. While it",
+                     "is true that the average Nordic country spends a higher",
+                     "percentage of its GDP on education, the actual",
+                     "figures are surprising.")
+    message <- paste(message,
+                     "On average, a Nordic country spends",
+                     avg_nordic_expenditure,
+                     "billion dollars on education, while a country with a top GDP spends",
+                     avg_top_economy_expenditure,
+                     "billion dollars on education.")
+  })
+  
+  output$nordic_reasons <- renderText({
+    message <- "Of course, population is a factor."
+    message <- paste(message,
+                     "The population of the average Nordic country is",
+                     avg_nordic_pop,
+                     "people. However, Iceland only has a population",
+                     "of about 300,000. Thus, excluding Iceland, the average",
+                     "Nordic country has a population of",
+                     avg_nordic_pop_no_iceland,
+                     "people. This population is tiny compared to the average",
+                     "top economy country, which has",
+                     avg_top_economy_pop,
+                     "people.")
+    message <- paste(message,
+                     "However, while the average top economy country has",
+                     round(avg_top_economy_pop / avg_nordic_pop_no_iceland),
+                     "times more people than the average Nordic country,",
+                     "they only spend",
+                     round(avg_top_economy_expenditure / avg_nordic_expenditure),
+                     "times as much on education.")
+  })
+  
+  output$developing_plot <- renderPlot({
+    create_developing_plot(input$developing_country)
+  })
 }
--- app_ui.R
+++ app_ui.R
@@ -33,30 +33,40 @@ app_ui <- navbarPage(
   ),
   
   tabPanel("Developing Countries",
-    selectInput("developing_country", label = "Developing Country to Display",
-                choices = c(
-                  "All", "Afghanistan", "Ethiopia", "Philippines", "Indonesia", "Nepal"
-                )),
-    
-    selectInput("industrialized_country", label = "Industrialized Country to Display",
-                choices = c(
-                  "All", "Germany", "India", "Japan", "United Kingdom", "United States"
-                ))
-  ),
-  
-  tabPanel("About"),
-  tabPanel(
-    "Education spending versus population",
     div(
       style = "display:inline-block",
-      selectInput(inputId = "country", label = "Country of Choice",
-                  choices = select_values, selected = "United States")
-      ),
-    div(
-      style = "display:inline-block",
-      sliderInput(inputId = "year", label = "Year Range:",
-                  min = 1970, max = 2018, value = c(2002, 2017)),
+      selectInput("developing_country", label = "Developing Country to Display",
+                  choices = c(
+                    "All", "Afghanistan", "Ethiopia", "Philippines", "Indonesia", "Nepal"
+                  ))
     ),
-    plotOutput("education_plot"),
+    
+    plotOutput("developing_plot"),
+    
+    div(
+      style = "display:inline-block",
+      selectInput("industrialized_country", label = "Industrialized Country to Display",
+                  choices = c(
+                    "All", "Germany", "India", "Japan", "United Kingdom", "United States"
+                  ))
+    ),
+    
+    plotOutput("industrialized_plot")
   )
+  
+  # tabPanel("About"),
+  # tabPanel(
+  #   "Education spending versus population",
+  #   div(
+  #     style = "display:inline-block",
+  #     selectInput(inputId = "country", label = "Country of Choice",
+  #                 choices = select_values, selected = "United States")
+  #     ),
+  #   div(
+  #     style = "display:inline-block",
+  #     sliderInput(inputId = "year", label = "Year Range:",
+  #                 min = 1970, max = 2018, value = c(2002, 2017)),
+  #   ),
+  #   plotOutput("education_plot"),
+  # )
 )
